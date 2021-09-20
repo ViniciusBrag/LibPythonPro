1b79f771b92759feee41f12c94037253a00fb177
viniciusbraga <Vbragadev@gmail.com>
2021-09-20 19:31:12 -0300

    commit

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ def find_package_data(
     return out
 
 
-PACKAGE = "LibPythonPro"
+PACKAGE = "LibPythonPro/LibPythonPro"
 NAME = PACKAGE
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "Vinicius Braga"
