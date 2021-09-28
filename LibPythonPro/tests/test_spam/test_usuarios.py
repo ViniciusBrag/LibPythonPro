a269cb4932d9f2fbbdca443c9013a5d17cec71bd
viniciusbraga <Vbragadev@gmail.com>
2021-09-27 22:49:43 -0300

    Update requirements-dev.txt

---
+++ LibPythonPro/tests/test_spam/test_usuarios.py
@@ -0,0 +1,23 @@
+from LibPythonPro.spam.models import Usuario
+
+from LibPythonPro.tests.test_spam.confteste import *
+
+
+def test_salvar_usuario(sessao):
+    # setup
+    usuario = Usuario(nome='Vinicius', email='Vbragadev@gmail.com')
+    sessao.salvar(usuario)
+    assert isinstance(usuario.id, int)
+
+
+def test_listar_usuario(conexao, sessao):
+    usuarios = [
+        Usuario(nome='Vinicius', email='Vbragadev@gmail.com'),
+        Usuario(nome='Renzo', email='Vbragadev@gmail.com')
+    ]
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    assert usuarios == sessao.listar()
+    sessao.roll_back()
+    sessao.fechar()
+    conexao.fechar()
